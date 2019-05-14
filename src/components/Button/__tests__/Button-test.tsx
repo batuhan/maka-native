f49b83110872edc04100ee8cd38989545dee5c4c
Eda Bulut Ağırman <edabuluta3@gmail.com>
2019-05-14 19:20:12 +0300

    snapshots updated

--- src/components/Button/__tests__/Button-test.tsx
+++ src/components/Button/__tests__/Button-test.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import { shallow } from "enzyme";
 import Button from "../Button";
 import { Text } from "react-native";
+import renderer from "react-test-renderer";
 
 describe("Button", () => {
   describe("Rendering", () => {
     it("should match to snapshot", () => {
-      const component = shallow(
-        <Button onPress={() => {}}>
-          <Text>Button Text</Text>
-        </Button>
-      );
-      expect(component).toMatchSnapshot();
+      const children = <Text>Button Children</Text>;
+      const tree = renderer
+        .create(<Button onPress={() => {}}>{children}</Button>)
+        .toJSON();
+      expect(tree).toMatchSnapshot();
     });
   });
 });
--- src/components/FormInput/__tests__/FormInput-test.tsx
+++ src/components/FormInput/__tests__/FormInput-test.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { TextInputProps } from "react-native";
 import { shallow } from "enzyme";
 import FormInput from "../FormInput";
-
+import renderer from "react-test-renderer";
 describe("FormInput", () => {
   describe("Rendering", () => {
     it("should match to snapshot", () => {
       const placeholder = "form input placeholder";
-      const component = shallow(<FormInput placeholder={placeholder} />);
-      expect(component).toMatchSnapshot();
+      const tree = renderer
+        .create(<FormInput placeholder={placeholder} />)
+        .toJSON();
+      expect(tree).toMatchSnapshot();
     });
   });
 });
