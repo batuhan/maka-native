bc612d953ef18e309831f8d8307ea60e9f28bcb2
Batuhan Ozdemir <batuhanwilhelm@gmail.com>
2019-05-20 16:17:36 +0300

    Remove side margins from Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 const Container = styled(View)`
   justify-content: center;
   align-items: center;
-  margin: 5px;
+  margin: 5px 0;
   padding: 10px;
   border: 1px solid #f1f1f1;
   border-radius: 5px;
