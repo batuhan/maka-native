ade55e0bb28a10b0633bb611c6089a6a311a654a
Batuhan Ozdemir <batuhanwilhelm@gmail.com>
2019-05-20 13:49:15 +0300

    Write Alert component with styled

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,11 +1,33 @@
-import React from "react";
-import { Text } from "react-native";
-import styled from "styled-components";
+import React from 'react'
+import { View, Text } from 'react-native'
+import styled from 'styled-components'
 
-interface Props {
-  message: string;
+const typeMap = {
+  success: 'green',
+  warning: 'orange',
+  error: 'red',
 }
 
-export default function Alert({ message }: Props) {
-  return <Text>{message}</Text>;
+const Container = styled(View)<{ type: 'success' | 'warning' | 'error' }>`
+  text-align: center;
+  padding: 10px;
+  background-color: ${({ type }) => typeMap[type]};
+`
+
+const AlertText = styled(Text)`
+  color: #ffffff;
+  text-align: center;
+`
+
+interface Props {
+  message: string
+  type: 'success' | 'warning' | 'error'
+}
+
+export default function Alert({ type, message }: Props) {
+  return (
+    <Container type={type}>
+      <AlertText>{message}</AlertText>
+    </Container>
+  )
 }
