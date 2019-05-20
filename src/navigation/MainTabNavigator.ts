60ee1acd3f9a099a1226b907f230f303a5f9b154
Batuhan Ozdemir <batuhanwilhelm@gmail.com>
2019-05-20 12:50:26 +0300

    Authstack navigator

--- src/navigation/MainTabNavigator.ts
+++ src/navigation/MainTabNavigator.ts
@@ -1,54 +1,57 @@
-import * as React from "react";
+import * as React from 'react'
 
-import { Home } from "../screens/Home";
-import { Profile } from "../screens/Profile";
-import { Login, Register } from "../screens/Auth";
+import { Home } from '../screens/Home'
+import { Profile } from '../screens/Profile'
+import { Login, Register } from '../screens/Auth'
 
 import {
   createBottomTabNavigator,
   createStackNavigator,
-  createDrawerNavigator
-} from "react-navigation";
+  createDrawerNavigator,
+} from 'react-navigation'
 
 const HomeStackNavigator = createStackNavigator(
   {
     Home: {
       screen: Home,
       navigationOptions: () => ({
-        headerTitle: "Home"
-      })
+        headerTitle: 'Home',
+      }),
     },
-    Profile: {
-      screen: Profile,
-      navigationOptions: () => ({
-        headerTitle: "Profile"
-      })
-    },
-
-    Login: {
-      screen: Login,
-      navigationOptions: () => ({
-        title: "Login"
-      })
-    },
-    Register: {
-      screen: Register,
-      navigationOptions: () => ({
-        title: "Register"
-      })
-    }
   },
   {
     //headerMode: "none"
-  }
-);
+  },
+)
+
+const AuthStackNavigator = createStackNavigator({
+  Profile: {
+    screen: Profile,
+    navigationOptions: () => ({
+      headerTitle: 'Profile',
+    }),
+  },
+  Login: {
+    screen: Login,
+    navigationOptions: () => ({
+      title: 'Login',
+    }),
+  },
+  Register: {
+    screen: Register,
+    navigationOptions: () => ({
+      title: 'Register',
+    }),
+  },
+})
 
 const AppNavigator = createDrawerNavigator({
-  Home: { screen: HomeStackNavigator }
-});
+  Home: { screen: HomeStackNavigator },
+  Profile: { screen: AuthStackNavigator },
+})
 const BottomNavigator = createBottomTabNavigator({
   Home: AppNavigator,
-  Profile: AppNavigator
-});
+  Profile: AuthStackNavigator,
+})
 
-export default BottomNavigator;
+export default BottomNavigator
